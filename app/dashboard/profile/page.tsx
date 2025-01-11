ce45b3c18d4e57b9805762b780765608331983ca
sharundas <123480220+sharundas@users.noreply.github.com>
2025-01-11 21:35:59 +0530

    again small fix

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex justify-center items-center h-screen text-3xl'>
+      <h1>Currently Not Available</h1>
+    </div>
+  )
+}
+
+export default page
